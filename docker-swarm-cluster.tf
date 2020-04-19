24b057156c59f4d4fe1a72058067e413a73ee7b1
Carlos Hacks <hacks@lavid.ufpb.br>
2020-04-19 20:08:46 -0300

    Terraform cluster now configured with variables. Docker installed using user_data cloud-init file.

--- docker-swarm-cluster.tf
+++ docker-swarm-cluster.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region                  = "us-east-2"
-  shared_credentials_file = "var.credentials_dir"
-  profile                 = "var.credentials_profile"
+  shared_credentials_file = var.credentials_file
+  profile                 = var.credentials_profile
 }
 
 resource "aws_instance" "swarm-master" {
   ami                    = "ami-07c1207a9d40bc3bd"
-  instance_type          = "t3a.small"
-  vpc_security_group_ids = ["sg-2e311957"]
-  key_name               = "hacks-mbp-aws"
-  user_data              = file("install_docker.sh")
+  instance_type          = "t3a.micro"
+  vpc_security_group_ids = ["sg-2e311957","sg-0948b071add88d464"]
+  key_name               = var.key_name
+  user_data              = file("install_docker.conf")
 
   root_block_device {
     volume_type           = "gp2"
@@ -18,7 +18,7 @@ resource "aws_instance" "swarm-master" {
   }
 
   tags = {
-    name = "sefazpb"
+    name = "swarm-master"
   }
 }
 
@@ -26,9 +26,9 @@ resource "aws_instance" "swarm-node" {
   count                  = 3
   ami                    = "ami-07c1207a9d40bc3bd"
   instance_type          = "t3a.micro"
-  vpc_security_group_ids = ["sg-2e311957"]
-  key_name               = "hacks-mbp-aws"
-  user_data              = file("install_docker.sh")
+  vpc_security_group_ids = ["sg-2e311957","sg-0948b071add88d464"]
+  key_name               = var.key_name
+  user_data              = file("install_docker.conf")
 
   root_block_device {
     volume_type           = "gp2"
@@ -37,14 +37,6 @@ resource "aws_instance" "swarm-node" {
   }
 
   tags = {
-    name = "sefazpb"
+    name = "swarm-nodes"
   }
-}
-
-variable "credentials_dir" {
-  type=string
-}
-
-variable "credentials_profile" {
-  type=string
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "credentials_file" {
+  type=string
+}
+
+variable "credentials_profile" {
+  type=string
+}
+
+variable "key_name" {
+  type=string
+}
